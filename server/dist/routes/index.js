a09c8516c97dd2691630674000155fd32f5e63b5
Emily <eholt179@gmail.com>
2025-05-20 12:52:16 -0600

    trying to fix error in page loading

--- server/dist/routes/index.js
+++ server/dist/routes/index.js
@@ -3,6 +3,6 @@ import authRoutes from './auth-routes.js';
 import apiRoutes from './api/index.js';
 import { authenticateToken } from '../middleware/auth.js';
 const router = Router();
-router.use('/auth', authRoutes);
+router.use('/api/auth', authRoutes);
 router.use('/api', authenticateToken, apiRoutes);
 export default router;
